37d5c13a4d5109429adb5246b3ae86e277d89a58
MarcusJones <jones.0bj3@gmail.com>
2019-02-10 10:13:10 +0100

    refactor imports

--- src/0_env/environment_00.py
+++ src/0_env/environment_00.py
@@ -1,13 +1,27 @@
+#%%
+import os
+
+import warnings
+import gc
+warnings.simplefilter("ignore", category=DeprecationWarning)
+
+from pathlib import Path
+#%%
 import numpy as np
 import pandas as pd
-import os
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.image as mplimg
 from matplotlib.pyplot import imshow
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
 
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.preprocessing import OneHotEncoder
+import sklearn as sk
+import sklearn.preprocessing
+
+#%%
+import keras as ks
 from keras import layers
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
@@ -17,7 +31,5 @@ from keras.models import Model
 
 import keras.backend as K
 from keras.models import Sequential
-from pathlib import Path
-import warnings
-import gc
-warnings.simplefilter("ignore", category=DeprecationWarning)
+
+#%%
--- src/1_data/data_00_load.py
+++ src/1_data/data_00_load.py
@@ -1,63 +1,13 @@
-# %% {"_uuid": "2cea35de3530cc898be5b85063b84e875401d092"}
+#%%
 
 PATH_INPUT = Path.cwd() / 'input'
 assert PATH_INPUT.exists()
 
-# %% {"_uuid": "46a8839e13a14eb8d16ea6823de9927ea63d5001"}
+# %%
 train_df = pd.read_csv(PATH_INPUT / 'train.csv')
 train_df.head()
 
+# %%
 
-# %% {"_uuid": "f46b24dbba74f22833cac6140e60348b15a8e047"}
+train_df.sample(10)
 
-# data=train_df
-# m = train_df.shape[0]
-# dataset = "train"
-
-
-
-def prepareImages(data, m, dataset):
-    print("Preparing images")
-    X_train = np.zeros((m, 100, 100, 3))
-    count = 0
-
-    for fig in data['Image']:
-        # load images into images of size 100x100x3
-        img = image.load_img(PATH_INPUT / dataset / fig, target_size=(100, 100, 3))
-        x = image.img_to_array(img)
-        x = preprocess_input(x)
-
-        X_train[count] = x
-        if (count % 500 == 0):
-            print("Processing image: ", count + 1, ", ", fig)
-        count += 1
-
-    return X_train
-
-
-# %% {"_uuid": "6587a101b58af064af0f9c60a1070c6c8f52d45f"}
-def prepare_labels(y):
-    values = np.array(y)
-    label_encoder = LabelEncoder()
-    integer_encoded = label_encoder.fit_transform(values)
-    # print(integer_encoded)
-
-    onehot_encoder = OneHotEncoder(sparse=False)
-    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-    # print(onehot_encoded)
-
-    y = onehot_encoded
-    # print(y.shape)
-    return y, label_encoder
-
-
-# %% {"_uuid": "4afe4128a0cd6859848c8a80686208082d647c39"}
-X = prepareImages(train_df, train_df.shape[0], "train")
-X /= 255
-
-# %% {"_uuid": "675924f8863aef27cf90dc668e0a68cd609dfc1c"}
-y, label_encoder = prepare_labels(train_df['Id'])
-
-# %% {"_uuid": "14d243b19023e830b636bea16679e13bc40deae6"}
-y.shape
