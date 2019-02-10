f9541037fc09d3385f7a401cd364a74374b68624
MarcusJones <jones.0bj3@gmail.com>
2019-02-10 13:53:35 +0100

    update plots

--- src/1_data/data_00_load.py
+++ src/1_data/data_00_load.py
@@ -26,7 +26,7 @@ font_steering = {'family': 'monospace',
 
 
 #%%
-ROWS = 2
+ROWS = 3
 COLS = 3
 NUM_IMAGES = ROWS * COLS
 
@@ -35,13 +35,20 @@ sel_img_paths = [PATH_INPUT / 'train' / name for name in sel_img_fnames]
 assert all([p.exists() for p in sel_img_paths])
 assert len(sel_img_paths) % 2 == 0
 #%%
-#%%
 fig = plt.figure(figsize=PAPER['A3_LANDSCAPE'], facecolor='white')
 fig.suptitle("Test {}".format('TEst'), fontsize=20)
 
 for i, img_path in enumerate(sel_img_paths):
     logging.info("{}".format(img_path))
     ax = fig.add_subplot(ROWS, COLS, i + 1)
+    img = mpl.image.imread(img_path)
+    ax.imshow(img)
+    ax.axis('off')
+    # plt.title(str_label)
+plt.show()
+# outpath = os.path.join(dataset.path_dataset, outfname)
+# fig.savefig(outpath)
+# logging.debug("Wrote Sample Frames figure to {}".format(outpath))
 
 #%%
 
