fc6671c7495701ff64b9b9b99a421333610e9f3f
MarcusJones <jones.0bj3@gmail.com>
2019-03-15 16:46:36 +0100

    add conversion script

---
+++ scripts/convert_all.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import jupytext
+
+references_path = Path.cwd() / "reference notebooks"
+assert references_path.exists()
+references_scripts_path = Path.cwd() / "reference scripts"
+assert references_scripts_path.exists()
+
+notebooks = [p for p in references_path.iterdir() if p.suffix == ".ipynb"]
+scripts = [p for p in references_path.iterdir() if p.suffix == ".py"]
+
+
+
+for f in notebooks:
+
+    converted_files = [p.stem for p in references_scripts_path.iterdir()]
+    if f.stem in converted_files:
+        print(f, "already exists, skip")
+
+    print(f)
+jupytext.readf(nb_file, fmt=None)
